3ed3415bd32c00f375631a841d41cbb0b1b57da0
Md Ashikur Rahman <50617850+ashikur7748@users.noreply.github.com>
2022-03-22 13:12:07 +0600

    filedwonload.blade.php

---
+++ filedwonload.blade.php
@@ -0,0 +1,22 @@
+
+public function ZipCreateAndMultipleFileDownload($files){ 
+
+       $filearrayconvert = explode(",", $files);
+       //$filearray = array("word.pdf","textfile.txt",); demo
+        $zip = new ZipArchive();
+        $zip_name = "WebApp_DocMS_".date("d_m_y h-i-sa").".zip"; // Zip name
+        $zip->open($zip_name,  ZipArchive::CREATE);
+        foreach ($filearrayconvert as $file) {
+         $path = public_path('DocMS/'.$file);
+        if(file_exists($path)){
+        $zip->addFromString(basename($path),  file_get_contents($path));  
+        }
+        else{
+        echo"file does not exist";
+        }
+        }
+        $zip->close();
+
+        return response()->download($zip_name);
+
+    }
